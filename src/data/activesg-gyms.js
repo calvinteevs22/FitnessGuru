15075909e4e5a6451db5bca1db6604c476e10c6d
calvinteevs22 <calvinteevs22@gmail.com>
2026-05-15 00:44:50 +0800

    feat: add ActiveSG gym list by region

---
+++ src/data/activesg-gyms.js
@@ -0,0 +1,39 @@
+// src/data/activesg-gyms.js
+
+export const ACTIVESG_REGIONS = ['Central', 'East', 'North', 'Northeast', 'West']
+
+export const ACTIVESG_GYMS = {
+  Central: [
+    'ActiveSG Gym @ Bishan Sports Centre',
+    'ActiveSG Gym @ Delta Sports Centre',
+    'ActiveSG Gym @ Jalan Besar Sports Centre',
+    'ActiveSG Gym @ Kallang Leisure Park',
+    'ActiveSG Gym @ Queenstown Sports Centre',
+    'ActiveSG Gym @ Toa Payoh Sports Centre',
+  ],
+  East: [
+    'ActiveSG Gym @ Bedok Sports Centre',
+    'ActiveSG Gym @ Kallang Sports Hub',
+    'ActiveSG Gym @ Pasir Ris Sports Centre',
+    'ActiveSG Gym @ Tampines Sports Centre',
+  ],
+  North: [
+    'ActiveSG Gym @ Admiralty',
+    'ActiveSG Gym @ Sembawang Sports Centre',
+    'ActiveSG Gym @ Woodlands Civic Centre',
+    'ActiveSG Gym @ Yishun Sports Centre',
+  ],
+  Northeast: [
+    'ActiveSG Gym @ Ang Mo Kio Sports Centre',
+    'ActiveSG Gym @ Hougang Sports Centre',
+    'ActiveSG Gym @ Punggol Sports Centre',
+    'ActiveSG Gym @ Sengkang Sports Centre',
+  ],
+  West: [
+    'ActiveSG Gym @ Bukit Batok Sports Centre',
+    'ActiveSG Gym @ Choa Chu Kang Sports Centre',
+    'ActiveSG Gym @ Clementi Sports Centre',
+    'ActiveSG Gym @ Jurong East Sports Centre',
+    'ActiveSG Gym @ Jurong West Sports Centre',
+  ],
+}
